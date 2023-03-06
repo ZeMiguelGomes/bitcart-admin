921961728d2ca50585a131d88cc866e0d16d8ac6
Zé Miguel Gomes <jose.miguel.gomes@hotmail.com>
2023-03-06 15:19:42 +0000

    Feature: Delete NFT

--- components/NFTCard.vue
+++ components/NFTCard.vue
@@ -1,42 +1,62 @@
 <template>
-  <v-card class="nft-card" outlined>
-    <v-img
-      :src="nftMedia"
-      :alt="nft.title"
-      height="300"
-      class="rounded-t-md"
-    ></v-img>
+  <div>
+    <v-card class="nft-card" outlined>
+      <v-img
+        :src="nftMedia"
+        :alt="nft.title"
+        height="300"
+        class="rounded-t-md"
+      ></v-img>
 
-    <v-card-text>
-      <h2 class="text-xl font-weight-bold mb-1">{{ nft.title }}</h2>
-      <p class="mb-2">
-        Id: {{ nft.id.tokenId.substr(nft.id.tokenId.length - 4) }}
-      </p>
-      <p class="mb-4">{{ contractAddressShortened }}</p>
-      <p class="text-gray-600">{{ nft.description.substr(0, 150) }}</p>
-      <div
-        v-for="attribute in nft.metadata.attributes"
-        :key="attribute.trait_type"
-      >
-        <span class="font-weight-bold">{{ attribute.trait_type }}: </span>
-        <span>{{ attribute.value }}</span>
-      </div>
-    </v-card-text>
-    <v-card-actions class="d-flex justify-center">
-      <v-btn color="primary" @click="openBlockExplorer">
-        View on Block Explorer
-      </v-btn>
-    </v-card-actions>
-    <v-card-actions>
-      <v-btn color="primary" @click="transferNFTPage(nft)">
-        Transfer NFT
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+      <v-card-text>
+        <h2 class="text-xl font-weight-bold mb-1">{{ nft.title }}</h2>
+        <p class="mb-2">
+          Id: {{ nft.id.tokenId.substr(nft.id.tokenId.length - 4) }}
+        </p>
+        <p class="mb-4">{{ contractAddressShortened }}</p>
+        <p class="text-gray-600">{{ nft.description.substr(0, 150) }}</p>
+        <div
+          v-for="attribute in nft.metadata.attributes"
+          :key="attribute.trait_type"
+        >
+          <span class="font-weight-bold">{{ attribute.trait_type }}: </span>
+          <span>{{ attribute.value }}</span>
+        </div>
+      </v-card-text>
+      <v-card-actions class="d-flex justify-center">
+        <v-btn color="primary" @click="openBlockExplorer">
+          View on Block Explorer
+        </v-btn>
+      </v-card-actions>
+      <v-card-actions>
+        <tooltip-icon
+          icon="mdi-delete"
+          text="Delete"
+          class="mr-2"
+          @click="deleteNFT()"
+        ></tooltip-icon>
+        <v-btn color="primary" @click="transferNFTPage(nft)">
+          Transfer NFT
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-snackbar
+      v-model="showSnackbar"
+      :timeout="2500"
+      color="success"
+      @input="onSnackbarInput"
+    >
+      {{ snackbarMessage }}
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
+import TooltipIcon from "@/components/TooltipIcon"
 export default {
+  components: {
+    TooltipIcon,
+  },
   props: {
     nft: {
       type: Object,
@@ -49,8 +69,24 @@ export default {
   },
   data() {
     return {
-      price: null,
-      networkName: "goerli",
+      showSnackbar: false,
+      snackbarMessage: "",
+      snackbarColor: "error",
+      abiData: {},
+      contractAddress: "",
+      fromAddress: "",
+    }
+  },
+  head() {
+    return {
+      script: [
+        {
+          src: `https://unpkg.com/@metamask/detect-provider/dist/detect-provider.min.js`,
+        },
+        {
+          src: `https://unpkg.com/web3@latest/dist/web3.min.js`,
+        },
+      ],
     }
   },
   computed: {
@@ -67,6 +103,7 @@ export default {
   },
   mounted() {
     console.log(this.nft)
+    this.fromAddress = localStorage.getItem("userAddress")
   },
 
   methods: {
@@ -104,6 +141,75 @@ export default {
         query: { nft: JSON.stringify(nft) },
       })
     },
+
+    showMessage(ok, text) {
+      this.snackbarMessage = text
+      this.snackbarColor = ok ? "success" : "error"
+      this.showSnackbar = true
+    },
+    showError(text) {
+      this.showMessage(false, text)
+    },
+    async fetchTokenABI() {
+      // Gets the ABI to create a transaction
+      await this.$axios.get(`/vouchers/nft/abi`).then((res) => {
+        this.abiData = res.data.ABI
+        this.contractAddress = res.data.contractAddress
+      })
+    },
+    async deleteNFT() {
+      await this.fetchTokenABI()
+      if (window.ethereum) {
+        this.web3 = new window.Web3(window.ethereum)
+
+        const contract = await new this.web3.eth.Contract(
+          this.abiData,
+          this.contractAddress
+        )
+
+        const tokenIdHex = this.web3.utils.toHex(this.nft.id.tokenId)
+
+        const functionSignature = contract.methods
+          .burnToken(this.web3.utils.toBN(tokenIdHex))
+          .encodeABI()
+
+        const gasPrice = await this.web3.eth.getGasPrice()
+
+        const gasEstimate = await contract.methods
+          .burnToken(tokenIdHex)
+          .estimateGas({ from: this.fromAddress })
+
+        const transactionParameters = {
+          to: contract.options.address,
+          from: this.fromAddress,
+          gasPrice: this.web3.utils.toHex(gasPrice),
+          gas: this.web3.utils.toHex(gasEstimate),
+          data: functionSignature,
+        }
+
+        // sign the transaction via MetaMask
+        try {
+          const txHash = await this.web3.eth.sendTransaction(
+            transactionParameters
+          )
+          console.log("Transaction hash: ", txHash)
+          this.showMessage(true, "Voucher Deleted successfully")
+        } catch (error) {
+          console.log("Error transferring NFT: ", error)
+          this.showError(false, error.message)
+        }
+        // After the transfer is completed wait 2 seconds and then refresh the page
+      } else {
+        window.location.href = "https://metamask.io/download"
+      }
+    },
+    onSnackbarInput(val) {
+      if (!val) {
+        setTimeout(() => {
+          window.location.reload()
+        }, 2500)
+      }
+    },
   },
 }
 </script>
@@ -112,5 +218,11 @@ export default {
 .nft-card {
   width: 300px;
   object-fit: cover;
+}
+
+.v-card__actions {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
 </style>
